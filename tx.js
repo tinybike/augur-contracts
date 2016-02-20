810611b642d824744080eadf0cccd6b2823730b1
Jack Peterson <jack@tinybike.net>
2016-02-19 17:03:08 -0800

    Added number returns type to incrementPeriod

--- tx.js
+++ tx.js
@@ -226,7 +226,8 @@ module.exports = function (network) {
             to: contracts.branches,
             method: "incrementPeriod",
             signature: "i",
-            send: true
+            send: true,
+            returns: "number"
         },
         addMarket: {
             to: contracts.branches,
