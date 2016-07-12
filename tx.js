d079e885e0b9517663f52590d50e5d30c87c640b
Jack Peterson <jack@tinybike.net>
2016-07-12 01:45:50 -0700

    Added static event info to api.json; added events to tx.js

--- tx.js
+++ tx.js
@@ -11,11 +11,18 @@ module.exports = function (network, contracts) {
     contracts = contracts || require("./contracts")[network || "2"];
     var api = clone(require("./api"));
 
-    for (var contract in api) {
-        if (!api.hasOwnProperty(contract)) continue;
-        for (var method in api[contract]) {
-            if (!api[contract].hasOwnProperty(method)) continue;
-            api[contract][method].to = contracts[contract];
+    for (var contract in api.functions) {
+        if (!api.functions.hasOwnProperty(contract)) continue;
+        for (var method in api.functions[contract]) {
+            if (!api.functions[contract].hasOwnProperty(method)) continue;
+            api.functions[contract][method].to = contracts[contract];
+        }
+    }
+    for (var contract in api.events) {
+        if (!api.events.hasOwnProperty(contract)) continue;
+        for (var method in api.events[contract]) {
+            if (!api.events[contract].hasOwnProperty(method)) continue;
+            api.events[contract][method].address = contracts[contract];
         }
     }
 
