1e4c6caba11acd56cda9e9a8bc7b1c5df76ed910
Paul Gebheim <paul@onetaste.us>
2017-11-14 16:39:15 -0800

    Update contracts code

---
+++ update-contracts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+npx dotsunited-merge-json $1/addresses.json $1/addresses.json $2/addresses.json > ./addresses.json
+cat $1/contracts.json | npx jqn --color=false -j "at('contracts') | map(values) | flatten | map(mapValues(get('abi'))) | reduce(merge, {})" > ./abi.json
+
+git add addresses.json abit.json
+git commit -m "Auto-updating from push to augur-core#${BRANCH} (${COMMIT})"
+
+case $BRANCH
+  "master")
+    echo "Update master of augur-contracts, manual NPM release needed"
+    git push
+    ;;
+  "develop")
+    echo "Updating develop branch of augur-contracts with force push"
+    git checkout -b $BRANCH origin/$BRANCH
+    git push --force-with-lease
+    ;;
+  *)
+    echo "Making new branch on augur-contracts to match ${BRANCH}"
+    git checkout -b core_branch_$BRANCH
+    git push --force-with-lease
+    ;;
+esac
