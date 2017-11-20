2f67f101fa17b3550bf15e1655dd080e179305c4
Paul Gebheim <paul@onetaste.us>
2017-11-20 02:25:42 -0800

    Use / as delimiter on tag

--- update-contracts.sh
+++ update-contracts.sh
@@ -27,7 +27,7 @@ if [[ "$AUTOCOMMIT" == "true" ]]; then
         # a new release
         echo "Update master of augur-contracts, and publishning new NPM version"
         npm version patch
-        git tag augur-core-$TAG # create a tag to match the augur-core tag
+        git tag augur-core/$TAG # create a tag to match the augur-core tag
         #git push && push --tags && npm publish
       fi
       ;;
